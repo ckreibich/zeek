3ee0bfb86404ab57eea94d9c94d5ffb013b44036
Christian Kreibich <christian@corelight.com>
2025-03-21 19:10:03 -0700

    Try out a derived tuple manager, honoring VLANs

--- src/conntuple/Manager.cc
+++ src/conntuple/Manager.cc
@@ -17,8 +17,52 @@ Manager::~Manager() {}
 
 ConnTuplePtr Manager::GetTuple(const Packet* pkt) { return std::make_shared<ConnTuple>(); }
 
-zeek::detail::ConnKeyPtr Manager::GetKey(const ConnTuple& tuple) { return std::make_shared<zeek::detail::ConnKey>(tuple); }
+zeek::detail::ConnKeyPtr Manager::GetKey(const ConnTuple& tuple) {
+    return std::make_shared<zeek::detail::ConnKey>(tuple);
+}
 
-void Manager::FillVal(RecordValPtr& tuple) { }
+void Manager::FillVal(RecordValPtr& tuple) {}
+
+
+struct VlanConnTuple : public ConnTuple {
+    uint32_t vlan = 0;
+};
+
+class VlanConnKey : public detail::ConnKey {
+public:
+    uint32_t vlan = 0;
+
+    VlanConnKey(const ConnTuple& conn) : detail::ConnKey(conn) {};
+    VlanConnKey(const VlanConnTuple& conn) : detail::ConnKey(conn) { vlan = conn.vlan; };
+
+    size_t PackedSize() const override { return detail::ConnKey::PackedSize() + sizeof(vlan); }
+
+    size_t Pack(uint8_t* data, size_t size) const override {
+        if ( size < PackedSize() )
+            return 0;
+
+        uint8_t* ptr = data;
+
+        ptr += detail::ConnKey::Pack(data, size);
+        memcpy(ptr, &vlan, sizeof(vlan));
+        ptr += sizeof(vlan);
+
+        return ptr - data;
+    }
+};
+
+ConnTuplePtr VlanAwareManager::GetTuple(const Packet* pkt) {
+    auto res = std::make_shared<VlanConnTuple>();
+    res->vlan = pkt->vlan;
+    return res;
+}
+
+zeek::detail::ConnKeyPtr VlanAwareManager::GetKey(const ConnTuple& tuple) {
+    const VlanConnTuple& vtuple = dynamic_cast<const VlanConnTuple&>(tuple);
+    auto res = std::make_shared<VlanConnKey>(tuple);
+    res->vlan = vtuple.vlan;
+    return res;
+}
+
 
 } // namespace zeek::conntuple
